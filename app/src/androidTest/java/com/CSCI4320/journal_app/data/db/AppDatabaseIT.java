3e2250d4c6a0e4eb2e7ccc6a2fd94d9dfaa0332d
Kenya Barnes <barneskenya@gmail.com>
2019-10-24 01:09:42 -0400

    improved integration test, added permissions, updated annotations package

--- app/src/androidTest/java/com/CSCI4320/journal_app/data/db/AppDatabaseIT.java
+++ app/src/androidTest/java/com/CSCI4320/journal_app/data/db/AppDatabaseIT.java
@@ -3,16 +3,37 @@ package com.CSCI4320.journal_app.data.db;
 import androidx.room.Room;
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 
+import com.CSCI4320.journal_app.data.model.User;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.Assert;
 import org.junit.runner.RunWith;
 
+import static androidx.test.InstrumentationRegistry.getContext;
+
 @RunWith(AndroidJUnit4ClassRunner.class)
 public class AppDatabaseIT {
 
+    AppDatabase db;
+
+    @Before
+    public void setup() {
+        db = Room.databaseBuilder(getContext(), AppDatabase.class, "test-database").build();
+    }
+
     @Test
-    public void createDatabase() {
-        AppDatabase db = Room.databaseBuilder(null, AppDatabase.class, "test-database").build();
-        Assert.assertNotNull(db);
+    public void insertUser() {
+        User user = new User();
+        user.userId = 1;
+        user.firstName = "kenya";
+        user.lastName = "barnes";
+
+        db.userDAO().insert(user);
+
+        User returnedUser = db.userDAO().getUser(1);
+        Assert.assertEquals(user.userId, returnedUser.userId);
+        Assert.assertEquals(user.firstName, returnedUser.firstName);
+        Assert.assertEquals(user.lastName, returnedUser.lastName);
     }
 }
